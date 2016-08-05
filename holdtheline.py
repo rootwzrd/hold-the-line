dc8c8c7139549490f279cc57da92d16796fe94f7
vitorio <email_about_holdtheline@vitor.io>
2016-08-05 04:58:27 -0500

    Simplified functionality

--- holdtheline.py
+++ holdtheline.py
@@ -23,100 +23,109 @@ import phonenumbers
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('holdtheline.cfg'))
-twilio_outgoing_message = config.get('holdtheline', 'twilio_outgoing_message')
-blocked_numbers = config.get('holdtheline', 'blocked_numbers').split(',')
-to_email = config.get('holdtheline', 'to_email')
-from_email = config.get('holdtheline', 'from_email')
-twilio_account_sid = config.get('holdtheline', 'twilio_account_sid')
-twilio_auth_token = config.get('holdtheline', 'twilio_auth_token')
+BLOCKED_NUMBERS = config.get('holdtheline', 'blocked_numbers').split(',')
+CALL_REDIRECT = config.get('holdtheline', 'call_redirect')
+BUTTON_SELECTION = config.get('holdtheline', 'button_selection')
+BUTTON_REDIRECT = config.get('holdtheline', 'button_redirect')
+BUTTONRETRY1_REDIRECT = config.get('holdtheline', 'buttonretry1_redirect')
+BUTTONRETRY2_REDIRECT = config.get('holdtheline', 'buttonretry2_redirect')
+BUTTONRETRY3_REDIRECT = config.get('holdtheline', 'buttonretry3_redirect')
+TO_EMAIL = config.get('holdtheline', 'to_email')
+FROM_EMAIL = config.get('holdtheline', 'from_email')
+TEXT_SUBJECT = config.get('holdtheline', 'text_subject')
+VOICEMAIL_SUBJECT = config.get('holdtheline', 'voicemail_subject')
+TWILIO_ACCOUNT_SID = config.get('holdtheline', 'twilio_account_sid')
+TWILIO_AUTH_TOKEN = config.get('holdtheline', 'twilio_auth_token')
 
-twilioclient = twilio.rest.TwilioRestClient(twilio_account_sid, twilio_auth_token)
+twilioclient = twilio.rest.TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 mailer = marrow.mailer.Mailer(dict(transport = dict(config.items('marrow.mailer'))))
 
 app = Flask(__name__)
 
-@app.route("/call", methods=['GET', 'POST'])
-def handle_call():
-    """When someone other than me dials in"""
-    from_number = request.values.get('From', None)
-    addons = request.values.get('AddOns', None)
-
-    reject = False
-
-    marchex = json.load(addons)
-    if 'code' in marchex:
-        if marchex['code'] == None:
-            if 'marchex_cleancall' in marchex['results']:
-                if 'status' in marchex['results']['marchex_cleancall']:
-                    if marchex['results']['marchex_cleancall']['status'] == 'successful':
-                        if marchex['results']['marchex_cleancall']['result']['result']['recommendation'] == 'BLOCK':
-                            reject = True
+def pass_number(number, addons):
+    """Check number validity"""
+    passnum = True
+    
+    try:
+        marchex = json.load(addons)
+        if marchex['results']['marchex_cleancall']['result']['result']['recommendation'] == 'BLOCK':
+            passnum = False
+    except:
+        pass
                             
     try:
-        gp = phonenumbers.parse(from_number)
+        gp = phonenumbers.parse(number)
     except:
-        reject = True
+        passnum = False
     else:
         if phonenumbers.is_possible_number(gp):
             if phonenumbers.is_valid_number(gp):
                 if gp.country_code == 1:
                     gpi = int(str(gp.national_number)[phonenumbers.phonenumberutil.length_of_national_destination_code(gp):])
-                    if gpi >= 5550100 or gpi <= 5550199:
-                        reject = True
+                    if gpi >= 5550100 and gpi <= 5550199:
+                        passnum = False
             else:
-                reject = True
+                passnum = False
         else:
-            reject = True
+            passnum = False
             
-    if from_number in blocked_numbers:
-        reject = True
+    if number in BLOCKED_NUMBERS:
+        passnum = False
         
+    return passnum
+
+@app.route("/call", methods=['GET', 'POST'])
+def handle_call():
+    """Check number validity and redirect or reject"""
+    from_number = request.values.get('From', None)
+    addons = request.values.get('AddOns', None)
+
     resp = twilio.twiml.Response()
-    if reject:
-        resp.reject()
+    
+    if pass_number(from_number, addons):
+        resp.redirect(CALL_REDIRECT)
     else:
-        resp.redirect(call_redirect)
+        resp.reject()
         
     return str(resp)
 
 @app.route("/text", methods=['GET', 'POST'])
 def handle_text():
-    """When someone other than me dials in"""
+    """Check number validity and reject or send email"""
     from_number = request.values.get('From', None)
-
-    resp = twilio.twiml.Response()
-    if from_number in blocked_numbers:
-        pass
-    else:
+    addons = request.values.get('AddOns', None)
+        
+    if pass_number(from_number, addons):
         to_number = request.values.get('To', None)
         sms_body = request.values.get('Body', None)
+        mms = request.values.get('NumMedia', None)
         mail_text = '''{} has a new text from {}.
 
 {}
 
 '''.format(to_number, from_number, sms_body)
-    
-    try:
-        mailer.start()
-        message = marrow.mailer.Message(author=from_email, to=to_email)
-        message.subject = '[hold-the-line] {} has a text from {}'.format(to_number, from_number)
-        message.plain = mail_text
-        mailer.send(message)
-        mailer.stop()
-    except:
-        e = sys.exc_info()
-        print 'A mailer error occurred: %s - %s' % (e[0], e[1])
-        raise
+
+        if mms:
+            mms = int(mms)
+            if mms > 0:
+                for m in range(0, mms):
+                    mc = request.values.get('MediaUrl{}'.format(m), None)
+                    mail_text = mail_text + '''Media content: {}
+'''.format(mc)
+        
+        try:
+            mailer.start()
+            message = marrow.mailer.Message(author=FROM_EMAIL, to=TO_EMAIL)
+            message.subject = TEXT_SUBJECT.format(from_num=from_number, to_num=to_number)
+            message.plain = mail_text
+            mailer.send(message)
+            mailer.stop()
+        except:
+            e = sys.exc_info()
+            print 'A mailer error occurred: %s - %s' % (e[0], e[1])
+            raise
 
     resp = twilio.twiml.Response()
-    return str(resp)
-
-@app.route("/voicemail", methods=['GET', 'POST'])
-def handle_voicemail():
-    """Goodbye caller"""
-
-    resp = twilio.twiml.Response()
-    resp.hangup()
     return str(resp)
 
 @app.route("/transcription", methods=['GET', 'POST'])
@@ -132,6 +141,7 @@ def handle_transcription():
 Recording: {}
 
 '''.format(to_number, from_number, voicemail)
+    
     if (transcript_status == "completed"):
         mail_text = mail_text + """Transcription:
 
@@ -140,8 +150,8 @@ Recording: {}
     
     try:
         mailer.start()
-        message = marrow.mailer.Message(author=from_email, to=to_email)
-        message.subject = '[hold-the-line] {} has voicemail from {}'.format(to_number, from_number)
+        message = marrow.mailer.Message(author=FROM_EMAIL, to=TO_EMAIL)
+        message.subject = VOICEMAIL_SUBJECT.format(from_num=from_number, to_num=to_number)
         message.plain = mail_text
         mailer.send(message)
         mailer.stop()
@@ -152,6 +162,26 @@ Recording: {}
     
     resp = twilio.twiml.Response()
     resp.hangup()
+    return str(resp)
+
+@app.route('/button', methods=['GET', 'POST'])
+def handle_button():
+    """Route based on a single button selection"""
+    digit_pressed = request.values.get('Digits', None)
+    retry_time = request.values.get('Retry', None)
+    
+    resp = twilio.twiml.Response()
+    
+    if int(digit_pressed) == int(BUTTON_SELECTION):
+        resp.redirect(BUTTON_REDIRECT)
+    else:
+        if int(retry_time) == 1:
+            resp.redirect(BUTTONRETRY1_REDIRECT)
+        elif int(retry_time) == 2:
+            resp.redirect(BUTTONRETRY2_REDIRECT)
+        else:
+            resp.redirect(BUTTONRETRY3_REDIRECT)
+    
     return str(resp)
 
 if __name__ == "__main__":
